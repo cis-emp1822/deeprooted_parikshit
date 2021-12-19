14d97d165fb49a82160ebf2e480c32d3ab50b0b1
p_pahare <p1pahare@techie.com>
2021-12-19 19:45:36 +0530

    Added repository for coins

--- lib/cubits/cubit/search_coins_cubit.dart
+++ lib/cubits/cubit/search_coins_cubit.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:deeprooted_parikshit/cubits/cubit/repositories/coins_repo.dart';
+import 'package:deeprooted_parikshit/models/bids_and_ask.dart';
 import 'package:deeprooted_parikshit/models/conversion_brief.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
@@ -6,5 +10,26 @@ import 'package:flutter/foundation.dart';
 part 'search_coins_state.dart';
 
 class SearchCoinsCubit extends Cubit<SearchCoinsState> {
-  SearchCoinsCubit() : super(SearchCoinsInitial());
+  SearchCoinsCubit({this.coinsRepo}) : super(SearchCoinsInitial());
+  final CoinsRepo? coinsRepo;
+
+  void getMeaningFromWord(
+      {String searchKeyword = '', bool showDetails = false}) async {
+    try {
+      if (searchKeyword.isNotEmpty) {
+        emit(LoadingState());
+        final movies = await coinsRepo!.getConversionBrief(searchKeyword);
+        emit(LoadedState(movies));
+      } else {
+        emit(ErrorState(
+            errorMessage:
+                "The user has denied the use of speech recognition."));
+        log("The user has denied the use of speech recognition.");
+      }
+      // some time later...
+
+    } catch (e) {
+      emit(ErrorState(errorMessage: "Something Went Wrong"));
+    }
+  }
 }
